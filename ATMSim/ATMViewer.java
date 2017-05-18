41239a37bc980c6daff9339f736c98fedbeaba11
School Computer <a@gmail.com>
2017-05-18 11:24:00 -0700

    APCS Assignments

--- ATMSim/ATMViewer.java
+++ ATMSim/ATMViewer.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * ATM simulator viewer and UI
  */
+
+//Videos to Watch
+    https://www.youtube.com/watch?v=mH1TltI61yU
 
 public class ATMViewer {
 
@@ -12,34 +16,45 @@ public class ATMViewer {
     public static void main(String[] args){
 
         ATM A = new ATM();
-        JButton getBalanceButton = new JButton("Get Current Account Balance");
-        JPanel panelA = new JPanel();
-        JButton getPINButton = new JButton();
-        JButton getAccountNumberButton = new JButton();
+        GridBagConstraints c = new GridBagConstraints();
 
         /** Create the UI Window*/
         JFrame window = new JFrame();
-        window.setName("ATM Simulator");
+        window.setTitle("ATM Simulator");
         window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         window.setSize(WIDTH, HEIGHT);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
-
-        window.add(panelA);
+        window.setBackground(Color.BLACK);
 
         /** Panel A and contents */
 
+        JPanel panelA = new JPanel();
         panelA.setLocation(0, 0);
-        panelA.setSize(200, 200);
-        panelA.add(getBalanceButton);
+        panelA.setSize(200, 500);
+        panelA.setBackground(Color.RED);
+        panelA.setLayout(new GridBagLayout());
+        window.add(panelA);
 
         /** Create button to get account balance */
-        getBalanceButton.setLocation(0, 0);
+        JButton getBalanceButton = new JButton();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 1;
         getBalanceButton.setSize(120, 30);
+        panelA.add(getBalanceButton, c);
+
+        /** Create button to confirm both the PIN and Account Number */
+        JButton enterButton = new JButton();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 0;
+        c.gridy = 0;
+        enterButton.setSize(120, 30);
+        panelA.add(enterButton, c);
 
 
     }
-    public void pressed(){
 
-    }
 }
